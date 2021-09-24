2b0617fa8b1d21e696a9ae506dcd94f3ec1d9241
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-09-23 23:11:38 -0300

    Uri 1075 and 1078

---
+++ Repetition/uri1075.py
@@ -0,0 +1,5 @@
+value = int(input())
+
+for num in range(1, 10001):
+    if num % value == 2:
+        print(num)
---
+++ Repetition/uri1078.py
@@ -0,0 +1,4 @@
+value = int(input())
+
+for num in range(1, 11):
+    print('{} x {} = {}'.format(num, value, value * num))
