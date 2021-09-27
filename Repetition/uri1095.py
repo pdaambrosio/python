6ee4c35217aea8c6b2c175c623b30a2b62e2f0fc
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-09-26 21:00:34 -0300

    Uri 1095-1096

---
+++ Repetition/uri1095.py
@@ -0,0 +1,7 @@
+I = 1
+J = 60
+
+while J >= 0:
+    print('I = {} J = {}'.format(I, J))
+    I += 3
+    J -= 5
---
+++ Repetition/uri1096.py
@@ -0,0 +1,9 @@
+I = 1
+J = 7
+
+while I <= 9:
+    while J >= 5:
+        print('I={} J={}'.format(I, J))
+        J -= 1
+    I += 2
+    J = 7
