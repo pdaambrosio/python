9fa75622e924d584d0a6f5751cfe50c3910278ea
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-10-19 07:15:03 -0300

    uri 1159 fix space

--- Repetition/uri1159.py
+++ Repetition/uri1159.py
@@ -1,7 +1,6 @@
 x = int(input())
 
 while x != 0:
-
     even_numbers = []
     loop = 0
     while loop < 5:
@@ -9,6 +8,5 @@ while x != 0:
             even_numbers.append(x)
             loop += 1
         x += 1
-
     print(sum(even_numbers))
     x = int(input())
