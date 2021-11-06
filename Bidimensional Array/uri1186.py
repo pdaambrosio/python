42e7fb40a0bb99f7129da1480a25b2022e118176
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-11-06 14:12:48 -0300

    uri 1186

---
+++ Bidimensional Array/uri1186.py
@@ -0,0 +1,21 @@
+character = input().upper()
+matrix = [[] for num in range(12)]
+
+sum_diagonal = 0
+next_column = 0
+count_for_average = 0
+
+for line in range(12):
+    for column in range(12):
+        value = float(input())
+        matrix[line].append(value)
+
+for line in reversed(range(12)):
+    next_column += 1
+    for column in range(next_column, 12):
+        sum_diagonal += matrix[line][column]
+        count_for_average += 1
+
+average = sum_diagonal / count_for_average
+
+print(round(sum_diagonal, 1) if character == 'S' else round(average, 1))
