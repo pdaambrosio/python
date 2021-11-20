0e2978ec72abb6ba752e9c17449ed84487d9509b
Paulo Daniel <pda.ambrosio@gmail.com>
2021-11-19 23:38:31 -0300

    uri 1589

--- Repetition/uri1564.py
+++ Repetition/uri1564.py
@@ -2,6 +2,5 @@ while True:
     try:
         n = int(input())
         print('vai ter duas!' if n >= 1 else 'vai ter copa!')
-
     except EOFError:
         break
---
+++ Repetition/uri1589.py
@@ -0,0 +1,5 @@
+loop = int(input())
+
+for i in range(loop):
+    [r1, r2] = map(int, input().split())
+    print(r1 + r2)
