62764bca8a1e8ec010f2a2ab0446cf79fbf0c88d
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-12-08 21:50:57 -0300

    uri 1924

---
+++ Unknow/uri1924.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+
+def victoria_indecision(number):
+    courses = []
+    for i in range(number):
+        courses.append(input())
+    print('Ciencia da Computacao')
+
+
+victoria_indecision(n)
