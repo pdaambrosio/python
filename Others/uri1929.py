3adfefb4f84f65b92d757982c41acba9f446885d
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-12-09 23:26:12 -0300

    uri 1929

---
+++ Others/uri1929.py
@@ -0,0 +1,22 @@
+def lengths(a, b, c):
+    if abs((b - c)) < a < (b + c):
+        if abs((a - c)) < b < (a + c):
+            if abs((a - b)) < c < (a + b):
+                return True
+    else:
+        return False
+
+
+def triangle(a, b, c, d):
+    if lengths(a, b, c) or lengths(a, b, d) or lengths(a, c, d) or lengths(b, c, d):
+        return 'S'
+    else:
+        return 'N'  
+
+
+def main():
+    [a, b, c, d] = map(int, input().split())
+    print(triangle(a, b, c, d))
+
+
+main()
