6152265ce221817d40de8bad0c39a2aa5b597ea8
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-12-12 13:32:40 -0300

    Pylint - issue code: redefined-builtin

--- Repetition/uri1132.py
+++ Repetition/uri1132.py
@@ -1,10 +1,10 @@
-sum = 0
+total_sum = 0
 values = []
 x = values.append(int(input()))
 y = values.append(int(input()))
 
 for value in range(min(values), max(values) + 1):
     if value % 13 != 0:
-        sum += value
+        total_sum += value
 
-print(sum)
+print(total_sum)
