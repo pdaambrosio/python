0248f8db159e3e57c6c513f073dda7c174f417a1
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-12-12 13:17:06 -0300

    Pylint - issue code: used-before-assignment

--- Repetition/uri1118.py
+++ Repetition/uri1118.py
@@ -1,19 +1,20 @@
-new_calculate = 1
+new_calc = 1
+
 while new_calc == 1:
     loop = 0
     average = 0
     while loop < 2:
         score = float(input())
-        if score >= 0 and score <= 10:
+        if 0 <= score <= 10:
             average += score / 2
-            loop +=1
+            loop += 1
         else:
             print('nota invalida')
     print(f'media = {average:.2f}')
-    
+
     print('novo calculo (1-sim 2-nao)')
     new_calc = int(input())
 
     while new_calc != 1 and new_calc != 2:
         print('novo calculo (1-sim 2-nao)')
-        new_calculate = int(input())
+        new_calc = int(input())
