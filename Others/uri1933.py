1e55553d68586cd7f2d942c48d39c611c2fe11a6
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-12-14 22:14:07 -0300

    uri 1933

---
+++ Others/uri1933.py
@@ -0,0 +1,15 @@
+def tri_du(n1, n2):
+    larger_value = n1
+
+    if n1 < n2:
+        larger_value = n2
+    
+    return larger_value
+
+
+def main():
+    [c1, c2] = map(int, input().split())
+    print(tri_du(c1, c2))
+
+
+main()
