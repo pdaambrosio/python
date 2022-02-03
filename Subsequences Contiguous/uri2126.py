429af8c0fabe76ba34f9b89b6654328b608069af
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-02-02 23:36:43 -0300

    uri 2126 variable adjustment

--- Subsequences Contiguous/uri2126.py
+++ Subsequences Contiguous/uri2126.py
@@ -1,24 +1,24 @@
 def searching_subsequences(seq1: str, seq2: str) -> str:
     amount: int = seq2.count(seq1)
-    
+
     if amount == 0:
         return 'Nao existe subsequencia\n'
     else:
         position: int = seq2.rfind(seq1)
         return f'Qtd.Subsequencias: {amount}\nPos: {position + 1}\n'
-        
+
 
 def main():
     case: int = 1
     while True:
         try:
-            N1 = input()
-            N2 = input()
+            n1: str = input()
+            n2: str = input()
             print(f'Caso #{case}:')
-            print(searching_subsequences(N1, N2))
+            print(searching_subsequences(n1, n2))
         except EOFError:
             break
-        
+
         case += 1
 
 
